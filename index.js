7024fe6f6bba67c510cf21ad0e4a9c6abb03decc
Senny <sennycho@gmail.com>
2023-05-23 21:11:53 +0900

    조세연 1차 병합

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import adminRouter from './router/admin/admin.js';
 import { config } from './config.js';
+import bodyParser from 'body-parser';
+
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.json());
 app.set('view engine', 'ejs');
--- router/admin/admin.js
+++ router/admin/admin.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRouter from './user.js';
 import announcementRouter from './announcement.js';
+import medicineRouter from './medicine.js'
 
 const router = express.Router();
 
 router.use('/user', userRouter);
 router.use('/announcement', announcementRouter);
+router.use('/medicine', medicineRouter);
 
 export default router;
