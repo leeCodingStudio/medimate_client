af3950b888d30bb53898851b977c388422223d63
leeCodingStudio <lehj2308@gmail.com>
2023-05-25 15:30:36 +0900

    0525 이현준

--- service/admin/drugstore.js
+++ service/admin/drugstore.js
@@ -21,7 +21,7 @@ export async function showAll(req, res) {
 export async function create(req, res) {
     const { P_NUM, P_NAME, P_ADDRESS, P_PHONE, P_MON_S, P_MON_C, P_TUE_S, P_TUE_C, P_WED_S, P_WED_C, P_THU_S, P_THU_C, P_FRI_S, P_FRI_C, P_SAT_S, P_SAT_C, P_SUN_S, P_SUN_C, P_HOLI_S, P_HOLI_C, P_LATI, P_LONGI } = req.body;
     const data = { P_NUM, P_NAME, P_ADDRESS, P_PHONE, P_MON_S, P_MON_C, P_TUE_S, P_TUE_C, P_WED_S, P_WED_C, P_THU_S, P_THU_C, P_FRI_S, P_FRI_C, P_SAT_S, P_SAT_C, P_SUN_S, P_SUN_C, P_HOLI_S, P_HOLI_C, P_LATI, P_LONGI }
-    fetch(config.base + '/admin/drugstore/' + P_NUM, {
+    fetch(config.base + '/admin/drugstore/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
