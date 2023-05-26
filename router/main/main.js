bdd27ae523c2f2c91c551bc6a3aef611ffd78440
leeCodingStudio <lehj2308@gmail.com>
2023-05-26 10:06:44 +0900

    admin0526

--- router/main/main.js
+++ router/main/main.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import loginRouter from './login.js';
 import indexRouter from './index.js';
+import calendarRouter from './calendar.js';
 
 const router = express.Router();
 
 
 router.use('/index',indexRouter)
 router.use('/login', loginRouter);
+
+router.use('/calendar', calendarRouter);
 
 export default router;
